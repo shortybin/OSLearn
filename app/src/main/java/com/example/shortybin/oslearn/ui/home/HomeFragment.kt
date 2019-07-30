8b4c10ca31a70246bd9b14770ef6bcc06f8db26a
shrotybin <18334786948@163.com>
2019-07-30 20:56:48 +0800

    首页添加ui

---
+++ app/src/main/java/com/example/shortybin/oslearn/ui/home/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.shortybin.oslearn.ui.home
+
+import com.example.shortybin.oslearn.R
+import luyao.util.ktx.base.BaseVMFragment
+import luyao.util.ktx.base.BaseViewModel
+
+/**
+ * Created by shortybin
+ * on 2019-07-29
+ */
+class HomeFragment : BaseVMFragment<BaseViewModel>() {
+    override fun getLayoutResId(): Int {
+        return R.layout.fragment_home
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+
+    }
+}
